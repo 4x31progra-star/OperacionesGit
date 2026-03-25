11aa58fabf8fdb7085925e57d7943f998b1bc44a
Compu15 <Compu15@Compu15>
2026-03-25 14:20:45 -0600

    Suma

---
+++ src/operacionesbasicas/Suma.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package operacionesbasicas;
+
+/**
+ *
+ * @author Compu15
+ */
+public class Suma {
+
+    int sumar(int A, int B) {
+        return A+B;
+    }
+    
+}
